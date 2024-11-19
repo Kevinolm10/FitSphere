820a7e27d4ca920b5eda424ee3535f047f6d7cf7
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-19 16:42:38 +0000

    Set debug to true

--- fitsphere/settings.py
+++ fitsphere/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s4v$#ubq-9f^1+iq*3)#5g!t+&p=i^p=4n65o+v@8#noo7#92z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kevinolm10-fitsphere-j0gyuxgv992.ws.codeinstitute-ide.net',
 '.herokuapp.com'
