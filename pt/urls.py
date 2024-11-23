df545d1bb9b2639c145c1de3256e2991b2c38b48
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-23 13:41:52 +0000

    Fix urls

--- pt/urls.py
+++ pt/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pt, name='index'),  # Home page for the pt app (renders index.html)
+    path('', views.pt, name='index'),  # Home page route
 ]
