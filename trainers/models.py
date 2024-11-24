ed796941ddaac82257f14bfb77556b70f6595f44
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-24 13:24:53 +0000

    Set up cloudinary

--- trainers/models.py
+++ trainers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User  
 from django.core.exceptions import ValidationError
+from cloudinary.models import CloudinaryField
+
 
 
 # Create your models here.
