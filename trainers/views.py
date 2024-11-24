906573ee3130a243187a0bbee09c7f4160d921c4
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-24 19:52:49 +0000

    Change view to query and render trainer data

--- trainers/views.py
+++ trainers/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Trainer
 
 # Create your views here.
 def trainers(request):
-    return render(request, 'trainers/trainers.html')
+    # Query the database to get all trainers
+    trainers = Trainer.objects.all()
+
+    # Pass the trainers to the template as context
+    return render(request, 'trainers/trainers.html', {'trainers': trainers})
