ded72317dcf8ea1d6a8f930775574cc40d9c2623
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-26 10:11:30 +0000

    Create view for trainer profiles

--- trainers/views.py
+++ trainers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Trainer
 
 # Create your views here.
@@ -8,3 +8,8 @@ def trainers(request):
 
     # Pass the trainers to the template as context
     return render(request, 'trainers/trainers.html', {'trainers': trainers})
+
+def trainer_profile(request, trainer_id):
+    # Get the trainer by ID, or return a 404 if not found
+    trainer = get_object_or_404(Trainer, id=trainer_id)
+    return render(request, 'trainer_profile.html', {'trainer': trainer})
