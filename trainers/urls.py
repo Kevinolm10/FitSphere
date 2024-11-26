2df296dfa5869863083f18d0f817e7bfbe97d826
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-26 10:10:54 +0000

    Create routing for trainer profile page

--- trainers/urls.py
+++ trainers/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.trainers, name='trainers'),  # URL pattern for the trainers page
+    path('trainer/<int:trainer_id>/', views.trainer_profile, name='trainer_profile'),
 ]
