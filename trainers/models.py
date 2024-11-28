6569147c0396740d8c2a762e3f316a1d58ac09b3
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-28 11:25:44 +0000

    Create rating system with stars for trainer feedback model. Add meta classes for both models with sorting. Create a system to get the average rating for each trainer

--- trainers/models.py
+++ trainers/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User  
+from django.contrib.auth.models import User
+from datetime import datetime
 from django.core.exceptions import ValidationError
 from cloudinary.models import CloudinaryField
+from django.db.models import Avg
 
 
 # Create your models here.
@@ -36,20 +38,46 @@ class Trainer(models.Model):
             'invalid': 'Last name must only contain alphabetic characters.'
         }
     )
-    age = models.IntegerField(validators=[validate_age])  # Custom age validation
+    """Custom age validation"""
+    age = models.IntegerField(validators=[validate_age])  
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # Removed `default=datetime.now`
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f"Trainer: {self.first_name} {self.last_name}"
 
+    def get_average_rating(self):
+        """Get the average rating of the trainer based on the related feedbacks."""
+
+        avg_rating = self.feedbacks.aggregate(Avg('rating'))  # Aggregate the average rating of all feedbacks
+        return avg_rating['rating__avg'] if avg_rating['rating__avg'] else 0.0  # Return 0.0 if no feedbacks
+
+
+    class Meta:
+        ordering = ['first_name', 'last_name', 'created_at']
 
 class TrainerFeedback(models.Model):
+    """ Comments and rating system for trainers """
+    STARS = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+)
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, related_name="feedbacks")
-    rating = models.FloatField()
+    rating = models.IntegerField(choices=STARS)
     comment = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Feedback from {self.user.username} for {self.trainer.first_name} {self.trainer.last_name}"
+
+        class Meta:
+            ordering = ['-created_at']  # Ordering feedback by created_at, newest first
+            indexes = [
+                models.Index(fields=['trainer', 'created_at'])  # Index feedback by trainer and date
+        ]
