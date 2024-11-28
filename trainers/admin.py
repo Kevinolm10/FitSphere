d9211a60192334bb8d8f3b7604ece933e3e1b93d
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-11-28 11:58:40 +0000

    Add trainerfeedback model to admin page. Add summernote to admin page

--- trainers/admin.py
+++ trainers/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Trainer
+from .models import TrainerFeedback
 
-# Register your models here.
 class TrainerAdmin(admin.ModelAdmin):
     # List of fields to display in the admin list view
     list_display = ('first_name', 'last_name', 'age', 'user', 'featured_image')
-
-    # Fields to filter by in the admin interface
     list_filter = ('age',)
-
-    # Enable search functionality for these fields
     search_fields = ('first_name', 'last_name')
-
-    # Default ordering in the admin list view
     ordering = ('last_name',)
-
-    # Specify the fields to display in the form view
     fields = ('user', 'first_name', 'last_name', 'age', 'description', 'featured_image')
 
 
+class TrainerFeedbackAdmin(SummernoteModelAdmin):
+    # List of fields to display in the admin list view
+    list_display = ('user', 'trainer', 'rating', 'created_at', 'updated_at') 
+    list_filter = ('trainer', 'rating')  
+    search_fields = ('user__username', 'trainer__first_name', 'trainer__last_name')  
+
+    summernote_fields = ('comment',)
+
+# Register your models here.
+
 admin.site.register(Trainer, TrainerAdmin)
+admin.site.register(TrainerFeedback, TrainerFeedbackAdmin)
