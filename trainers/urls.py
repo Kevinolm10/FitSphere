721e70c6131fea5b64ac82eb188b0b60a3a429c0
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-12-03 17:00:43 +0000

    Add URL routing for comment form

--- trainers/urls.py
+++ trainers/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.trainers, name='trainers'),  # URL pattern for the trainers page
     path('trainer/<int:trainer_id>/', views.trainer_profile, name='trainer_profile'),
+    path('trainer/<int:trainer_id>/feedback/', views.submit_feedback, name='submit_feedback'),
 ]
