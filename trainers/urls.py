1b205cf4b71e6db527b4adc42cd4fe49b3f525e1
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-12-05 14:34:12 +0000

    Change href routing

--- trainers/urls.py
+++ trainers/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path  
 from . import views
+
+app_name = 'trainers'
+
 urlpatterns = [
     path('', views.trainers, name='trainers'),  # URL pattern for the trainers page
     path('trainer/<int:trainer_id>/', views.trainer_profile, name='trainer_profile'),
