23a5d1d66da5c833f5ba1a547294f31b62f625eb
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-12-05 10:56:56 +0000

    Add url routing for allauth

--- fitsphere/urls.py
+++ fitsphere/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('pt/', include('pt.urls')),  # pt app URLs
+    path("accounts/", include("allauth.urls")),
     path('trainers/', include('trainers.urls')),  # trainers app URLs
     path('', include('pt.urls')),  # This will use the pt app for the homepage
 ]
