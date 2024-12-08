ffecdeaf6354305d227894ebd77d0b494acacbe1
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-12-08 17:21:47 +0000

    Refactor all views for cleaner code and fix edit_feedback function

--- trainers/forms.py
+++ trainers/forms.py
@@ -5,3 +5,10 @@ class TrainerFeedbackForm(forms.ModelForm):
     class Meta:
         model = TrainerFeedback
         fields = ['rating', 'comment']
+
+def clean_comment(self):
+        comment = self.cleaned_data.get('comment')
+        # You can add extra validations here if needed
+        if not comment:
+            raise forms.ValidationError('Comment cannot be empty.')
+        return comment
