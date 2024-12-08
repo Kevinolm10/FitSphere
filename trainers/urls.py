53796ab76276ea67f1cdd1e585a229fa66c4623e
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-12-08 17:22:54 +0000

    Fix url routing

--- trainers/urls.py
+++ trainers/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.trainers, name='trainers'),  # URL pattern for the trainers page
     path('trainer/<int:trainer_id>/', views.trainer_profile, name='trainer_profile'),
     path('feedback/delete/<int:id>/', views.delete_feedback, name='delete_feedback'),
-    path('feedback/edit/<int:id>/', views.edit_feedback, name='edit_feedback'),  
+    path('feedback/edit/<int:feedback_id>/', views.edit_feedback, name='edit_feedback'), 
     path('trainer/<int:trainer_id>/feedback/', views.submit_feedback, name='submit_feedback'),
 ]
