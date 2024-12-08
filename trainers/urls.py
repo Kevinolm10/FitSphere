ee0d710b7cf3f1845c925d9d94888e60d4957753
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-12-08 13:09:43 +0000

    Add the urlpattern to trainers app

--- trainers/urls.py
+++ trainers/urls.py
@@ -6,5 +6,7 @@ app_name = 'trainers'
 urlpatterns = [
     path('', views.trainers, name='trainers'),  # URL pattern for the trainers page
     path('trainer/<int:trainer_id>/', views.trainer_profile, name='trainer_profile'),
+    path('feedback/edit/<int:id>/', views.edit_feedback, name='edit_feedback'),
+    path('feedback/delete/<int:id>/', views.delete_feedback, name='delete_feedback'),
     path('trainer/<int:trainer_id>/feedback/', views.submit_feedback, name='submit_feedback'),
 ]
